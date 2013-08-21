7f801f016f133aa13e22d7615899fe70f96e918e
michael.hoyle <michael.hoyle@filemobile.com>
2013-08-21 16:25:05 -0400

    removed .output stuff for add

--- bin/add.sh
+++ bin/add.sh
@@ -2,14 +2,9 @@
 
 COMMAND="add"
 
-if [ ! -e $VIM_OUTPUT_FILE ]
-then
-	touch $VIM_OUTPUT_FILE
-fi
-
 if [ -z $1 ]
 then
 	echo "Please supply a file to add"
 else
-	$SOURCE_CONTROL $COMMAND $1 > $VIM_OUTPUT_FILE
+	$SOURCE_CONTROL $COMMAND $1
 fi
