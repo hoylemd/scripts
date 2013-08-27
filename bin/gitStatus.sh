e76df9c6b81c8731cbc9dd7eb7f9810e1d83748d
Michael Hoyle <hoylemd@gmail.com>
2013-08-27 17:28:32 -0400

    added optional output file printing

--- bin/gitStatus.sh
+++ bin/gitStatus.sh
@@ -2,4 +2,12 @@
 
 COMMAND="status -uno"
 
-$SOURCE_CONTROL $COMMAND > $VIM_OUTPUT_FILE
+if [ "$1" = "-f" ]
+then
+    if [ ! -e $VIM_OUTPUT_FILE ]
+    then
+        touch $VIM_OUTPUT_FILE
+    fi
+    $SOURCE_CONTROL $COMMAND > $VIM_OUTPUT_FILE
+else
+    $SOURCE_CONTROL $COMMAND
