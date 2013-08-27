d4c3bb04f00b2934ba352f4c1b7b62d73fe455d3
Michael Hoyle <hoylemd@gmail.com>
2013-08-27 17:36:30 -0400

    added option file output

--- bin/diffOnFile.sh
+++ bin/diffOnFile.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
 
 COMMAND="diff"
+NOFILE_MSG="Please supply a file to diff"
 
-$SOURCE_CONTROL $COMMAND $1 > $VIM_OUTPUT_FILE
+if [ -z $1 ]
+then
+    echo $NOFILE_MSG
+else
+    if [ "$1" = "-f" ]
+    then
+        if [ ! -e $VIM_OUTPUT_FILE ]
+        then
+            touch $VIM_OUTPUT_FILE
+        fi
+        if [ -z $2 ]
+        then
+            echo $NOFILE_MSG
+        else
+            $SOURCE_CONTROL $COMMAND $2 > $VIM_OUTPUT_FILE
+        fi
+    else
+        $SOURCE_CONTROL $COMMAND $1
+    fi
+fi
