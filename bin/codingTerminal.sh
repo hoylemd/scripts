a9e33b774e49a0ec37a0c5788c5496fb910fb3b9
michael.hoyle <michael.hoyle@filemobile.com>
2014-01-30 14:21:48 -0500

    improved the formatting a bit again

--- bin/codingTerminal.sh
+++ bin/codingTerminal.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 COMMAND="vim"
 COMMIT_TEMPLATE0="short description"
-COMMIT_TEMPLATE1="Bug #BUG_ID"
-COMMIT_TEMPLATE2="work_time: HOURS"
+COMMIT_TEMPLATE1="Bug #"
+COMMIT_TEMPLATE2="hours worked: 1"
 
 # set up the commit file
 COMMIT_FILE=$COMMIT_MESSAGE_FILE
