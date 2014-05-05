040337bfe4f269d1759c3825c892578fbaa1a0a4
michael.hoyle <michael.hoyle@filemobile.com>
2014-05-05 12:39:52 -0400

    updated branch to use proper git checkout -b

--- bin/branch.sh
+++ bin/branch.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-BRANCH="branch"
-CHECKOUT="checkout"
+CHECKOUT="checkout -b"
 
 if [ -z $1 ]
 then
 	$SOURCE_CONTROL $CHECKOUT master
 else
-    $SOURCE_CONTROL $BRANCH $1
 	$SOURCE_CONTROL $CHECKOUT $1
 fi
 
