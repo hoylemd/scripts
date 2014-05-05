6b3bd9672d87beb9eab5aad8adaae8810eaccd35
michael.hoyle <michael.hoyle@filemobile.com>
2014-05-05 12:42:27 -0400

    updated to use new commit message format

--- bin/codingTerminal.sh
+++ bin/codingTerminal.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 COMMAND="vim"
-COMMIT_TEMPLATE0="short description"
-COMMIT_TEMPLATE1="Bug #"
-COMMIT_TEMPLATE2="hours worked: 1"
+COMMIT_TEMPLATE0="Bug # short description"
+COMMIT_TEMPLATE1="hours worked: 1"
+COMMIT_TEMPLATE2="changes"
 
 # set up the commit file
 COMMIT_FILE=$COMMIT_MESSAGE_FILE
@@ -11,6 +11,7 @@ then
     echo $COMMIT_TEMPLATE0 >> $COMMIT_FILE
     echo "" >> $COMMIT_FILE
 	echo $COMMIT_TEMPLATE1 >> $COMMIT_FILE
+    echo "" >> $COMMIT_FILE
 	echo $COMMIT_TEMPLATE2 >> $COMMIT_FILE
 fi
 
