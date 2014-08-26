6758f6a286e00eb9ecdf7811a8d9dd43816029d1
Michael D. Hoyle <hoylemd@gmail.com>
2014-08-26 11:23:42 -0400

    improved install script to use a function so things get isntalled cleaner

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#change this to 'big' for 3 vim columns
+# change this to 'big' for 3 vim columns
 SCREEN_SIZE=big
 BIN_DIRECTORY=bin
 VIM_DIRECTORY=~/.vim
@@ -8,26 +8,40 @@ PY_DIRECTORY=py
 LINK_DIRECTORY=~/bin
 PY_LINK_DIRECTORY=~/py
 
+# script installer/updater
+# params: source, destination
+install () {
+
+    #remove the old version if ti exists
+    if [ -e $2 ]; then
+        rm $2
+    fi
+
+    ln -f $1 $2
+
+    return 0
+}
+
 if [ ! -d $LINK_DIRECTORY ]; then
 	mkdir $LINK_DIRECTORY
 else
     rm $LINK_DIRECTORY/*
 fi
 
-ln -f  $BIN_DIRECTORY/add.sh $LINK_DIRECTORY/a
-ln -f  $BIN_DIRECTORY/branch.sh $LINK_DIRECTORY/b
-ln -f  $BIN_DIRECTORY/blame.sh $LINK_DIRECTORY/bl
-ln -f  $BIN_DIRECTORY/checkIn.sh $LINK_DIRECTORY/ci
-ln -f  $BIN_DIRECTORY/codingTerminal.sh $LINK_DIRECTORY/ct
-ln -f  $BIN_DIRECTORY/diffOnFile.sh $LINK_DIRECTORY/d
-ln -f  $BIN_DIRECTORY/findInFiles.sh $LINK_DIRECTORY/f
-ln -f  $BIN_DIRECTORY/gitLog.sh $LINK_DIRECTORY/log
-ln -f  $BIN_DIRECTORY/gitStatus.sh $LINK_DIRECTORY/s
-ln -f  $BIN_DIRECTORY/gitPush.sh $LINK_DIRECTORY/p
-ln -f  $BIN_DIRECTORY/graph.sh $LINK_DIRECTORY/graph
-ln -f  $BIN_DIRECTORY/rainbow.sh $LINK_DIRECTORY/rainbow
-ln -f  $BIN_DIRECTORY/mysql.sh $LINK_DIRECTORY/sql
-ln -f  $BIN_DIRECTORY/jslintOnFile.sh $LINK_DIRECTORY/l
+install $BIN_DIRECTORY/add.sh $LINK_DIRECTORY/a
+install  $BIN_DIRECTORY/branch.sh $LINK_DIRECTORY/b
+install  $BIN_DIRECTORY/blame.sh $LINK_DIRECTORY/bl
+install  $BIN_DIRECTORY/checkIn.sh $LINK_DIRECTORY/ci
+install  $BIN_DIRECTORY/codingTerminal.sh $LINK_DIRECTORY/ct
+install  $BIN_DIRECTORY/diffOnFile.sh $LINK_DIRECTORY/d
+install  $BIN_DIRECTORY/findInFiles.sh $LINK_DIRECTORY/f
+install  $BIN_DIRECTORY/gitLog.sh $LINK_DIRECTORY/log
+install  $BIN_DIRECTORY/gitStatus.sh $LINK_DIRECTORY/s
+install  $BIN_DIRECTORY/gitPush.sh $LINK_DIRECTORY/p
+install  $BIN_DIRECTORY/graph.sh $LINK_DIRECTORY/graph
+install  $BIN_DIRECTORY/rainbow.sh $LINK_DIRECTORY/rainbow
+install  $BIN_DIRECTORY/mysql.sh $LINK_DIRECTORY/sql
+install  $BIN_DIRECTORY/jslintOnFile.sh $LINK_DIRECTORY/l
 
 if [ ! -d $PY_LINK_DIRECTORY ]; then
 	mkdir $PY_LINK_DIRECTORY
@@ -35,15 +49,15 @@ else
     rm $PY_LINK_DIRECTORY/*
 fi
 
-ln -f  $PY_DIRECTORY/formatSqlOutput.py $PY_LINK_DIRECTORY/fsql.py
-ln -f  $PY_DIRECTORY/blame.py $PY_LINK_DIRECTORY/blame.py
+install  $PY_DIRECTORY/formatSqlOutput.py $PY_LINK_DIRECTORY/fsql.py
+install  $PY_DIRECTORY/blame.py $PY_LINK_DIRECTORY/blame.py
 
 if [ ! -d $VIM_DIRECTORY ]; then
 	mkdir $VIM_DIRECTORY
 fi
 
 if [ "$SCREEN_SIZE" = "big" ]; then
-    ln -f  vim/codingEnvironment.vim $VIM_DIRECTORY/codingEnvironmentBig.vim
+    install  vim/codingEnvironmentBig.vim $VIM_DIRECTORY/codingEnvironment.vim
 else
-    ln -f  vim/codingEnvironment.vim $VIM_DIRECTORY/codingEnvironment.vim
+    install  vim/codingEnvironment.vim $VIM_DIRECTORY/codingEnvironment.vim
 fi
